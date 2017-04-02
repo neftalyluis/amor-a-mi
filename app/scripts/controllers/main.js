630e027e59496080b73af9b3498b9d78eb029ae3
Abimael Luis <abi@airmail.cc>
2017-04-02 14:52:29 -0500

    Cambios foto de perfil en MainController

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,4 +15,30 @@ angular.module('amorAMiApp')
       $anchorScroll();
     }
 
+    $scope.currentAuth = auth
+
+    $scope.path = $location.path()
+
+    $scope.urlProfile = "perfiles/";
+
+    $scope.uid = "";
+    $scope.showProfile = false;
+    $scope.showCourseName = false;
+    $scope.courseName = "";
+
+    $scope.$on('$locationChangeStart', function(event) {
+      if ($scope.currentAuth.$getAuth() !== null) {
+        $scope.uid = $scope.currentAuth.$getAuth().uid
+        $scope.showProfile = true
+      }
+      
+      if ($location.path().split("/")[3] === "lecciones") {
+        $scope.showCourseName = true;
+        var curso = $location.path().split("/")[2]
+        $scope.courseName = curso.charAt(0).toUpperCase() + curso.slice(1).replace("-", " ");
+        $scope.courseName = $scope.courseName.replace("-", " ");
+      }
+    });
+
+
   }]);
