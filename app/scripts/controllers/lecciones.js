b6ed27c84b06e924b62ca75e7e7f11c253cabeab
Abimael Luis <abimael.luis@vincoorbis.com>
2017-06-21 20:05:56 -0500

    Agregamos Modal de Felicitacion

--- app/scripts/controllers/lecciones.js
+++ app/scripts/controllers/lecciones.js
@@ -9,15 +9,23 @@
  */
 angular.module('amorAMiApp')
   .controller('LeccionesCtrl', ['auth', '$scope', '$location', '$routeParams', 'cursoRepository', 'avanceCursoRepository', 'currentAuth', function(auth, $scope, $location, $routeParams, cursoRepository, avanceCursoRepository, currentAuth) {
+
     $scope.cursoId = $routeParams.cursoId
     $scope.curso = cursoRepository($scope.cursoId)
-    $scope.progreso = '';
+    $scope.progreso = false;
     $scope.checked = avanceCursoRepository(currentAuth.uid)
 
-
-    $scope.openBienvenidaModal = function() {
-      //document.getElementById('bienvenidaModal').click();
-    }
+    $scope.checked.$loaded().then(function() {
+        $scope.curso.$loaded().then(function() {
+          if(Object.keys($scope.checked[$scope.curso.$id]).length===Object.keys($scope.curso.lecciones).length){
+            $('#felicitacionModal').modal()
+          }
+        }, function(error) {
+          console.log('Couldnt load curso')
+        })
+    }, function(error) {
+      console.log('Couldnt load progress')
+    })
 
     $scope.verLeccion = function(leccionIndex) {
       $location.path('/cursos/' + $scope.cursoId + '/lecciones/' + leccionIndex)
